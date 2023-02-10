aa53573e84e7d4d461fd22ff6af0d7907e432e99
apple <dan.hassan0910@gmail.com>
2023-02-10 06:26:43 +0100

    structure the home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 const HomeComponent = () => {
   return (
-    <div>
-      <h1 className="text-3xl">home component</h1>
+    <div className="container mx-auto px-4 py-4">
+      <h1 className="text-3xl text-center font-bold">Advanced React</h1>
+      <ul className="list-disc">
+        <li>
+          <Link to="/lazy">Lazy Loading</Link>
+        </li>
+        <li>
+          <Link to="/debouncing">Debouncing</Link>
+        </li>
+      </ul>
+      <div>
+        <button>Prev</button>
+        <button>Next</button>
+      </div>
     </div>
   )
 }
